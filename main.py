8d858cf82752bfd1cafb718825a7c1ea45cd42b4
syzzzang <syzz1105@naver.com>
2023-01-09 16:11:31 +0900

    edit main.py

--- main.py
+++ main.py
@@ -10,3 +10,22 @@ Iris_Data['target'] = Iris_Data['target'].map({0: "setosa", 1: "versicolor", 2: 
 
 X_Data = Iris_Data.iloc[:, :-1]
 Y_Data = Iris_Data.iloc[:, [-1]]
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.neural_network import MLPClassifier
+
+models = []
+models.append(('LR', LogisticRegression()))
+models.append(('DT', DecisionTreeClassifier()))
+models.append(('SVM', SVC()))
+models.append(('NB', GaussianNB()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('RF', RandomForestClassifier()))
+models.append(('GB', GradientBoostingClassifier()))
+models.append(('ANN', MLPClassifier()))
